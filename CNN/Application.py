8c678327d68458af5a71248d588e52995a269dcc
guinus22800 <guinus22800@hotmail.fr>
2017-12-21 15:18:46 -0500

    Application CNN

--- CNN/Application.py
+++ CNN/Application.py
@@ -10,6 +10,10 @@ import wave
 import pylab as pb
 import numpy as np
 import time
+import tkinter as tk
+from tkinter import filedialog
+from keras.preprocessing import image
+from keras.models import model_from_json
 
 
 #############Constante
@@ -17,6 +21,7 @@ import time
 FE = 22050
 #NFFT echantillonnage - length of the windowing segments
 NFFT = 1024
+
 
 #############Fonction get_Info du fichier audio
 #Return les info de l'audio et sa duree
@@ -49,71 +54,65 @@ def graph_spectrogram(wav_file, name):
     #pb.savefig('training_set/Base_spect_parole/' +name + '.png')
     #pb.savefig('training_set/Base_spect_musique/' +name + '.png')
     pb.savefig(name)
+    
+def callback():
+    global var
+    wav_file_parole = filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("wav files","*.wav"),("all files","*.*")))
+
+    #name_png = 'spectrogram_parole_' + str(i)
+    name_png = 'spectrogram.png'
+    graph_spectrogram(wav_file_parole, name_png)
+        
+    time.sleep(5) 
+        
+    ############################ Making a prediction ##################
+    # Load json and create model
+    json_file = open('Training/model_voice_music.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    
+    #load weights into new model
+    loaded_model.load_weights('Training/model_voice_music.h5')
+    print("Loaded model from disk")
+    prediction_image = image.load_img(name_png, target_size=(64, 64))
+
+    # passer l'image en tableau
+    prediction_image = image.img_to_array(prediction_image)
+
+    #test method
+    #mauvais format, il faut ajouter une dimension au test 
+    prediction_image = np.expand_dims(prediction_image, axis = 0)
+    loaded_model.predict(prediction_image)
+
+    #Correspondance 
+    result  = loaded_model.predict(prediction_image)
+    
+    if result[0][0] > 0: 
+        prediction='voice'
+    else:
+        prediction='music'
+        
+    var.set("Resultats de la prediction :\n" + prediction + "\n")
+
 
 #############Fonction main
 if __name__ == '__main__':
     
-        import tkinter as tk
-        from tkinter import filedialog
-
-        root = tk.Tk()
+    root = tk.Tk()
         
-        T = tk.Text(root, height=10, width=80)
-        T.pack()
-        T.insert(tk.END,"Classification parole/musique \n")
+    root.minsize(width=600, height=100)
         
-        root.filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("wav files","*.wav"),("all files","*.*")))
+    b = tk.Button(root, text="Open file", command=callback)
+    b.pack()
         
-        wav_file_parole = root.filename 
-       
-        #name_png = 'spectrogram_parole_' + str(i)
-        name_png = 'spectrogram.png'
-        graph_spectrogram(wav_file_parole, name_png)
+    #champ_label = tk.Label(root, text= "Resultats de la prediction :" + prediction)
+    var = tk.StringVar()
+    l = tk.Label(root, textvariable = var)
+    l.pack()
+    root.mainloop()
         
-        time.sleep(5) 
-        
-        ############################ Making a prediction ##################
-
-        from keras.preprocessing import image
-        from keras.models import model_from_json
-        
-        # Load json and create model
-        json_file = open('Training/model_voice_music.json', 'r')
-        loaded_model_json = json_file.read()
-        json_file.close()
-        loaded_model = model_from_json(loaded_model_json)
-        
-        #load weights into new model
-        loaded_model.load_weights('Training/model_voice_music.h5')
-        print("Loaded model from disk")
-
-        prediction_image = image.load_img(name_png, target_size=(64, 64))
-    
-    
-        # passer l'image en tableau
-        prediction_image = image.img_to_array(prediction_image)
-        
-        #test method
-        #mauvais format, il faut ajouter une dimension au test 
-        prediction_image = np.expand_dims(prediction_image, axis = 0)
-        loaded_model.predict(prediction_image)
-        
-        #Correspondance 
-        result  = loaded_model.predict(prediction_image)
-        
-        # 1 est une voix
-        #t = train_generator.class_indices
-        
-        if result[0][0] > 0: 
-            prediction='voice'
-        else:
-            prediction='music'
-        
-        #champ_label = tk.Label(root, text= "Resultats de la prediction :" + prediction)
-        T.insert(tk.END,"Resultats de la prediction :\n" + prediction + "\n")
-        root.mainloop()
-        
-        
+      
         
         
         
